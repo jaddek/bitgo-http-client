fb9bdc1ac94c6d9b3540cf8554bb0be3a2d5ec60
tonynazarov <tonynazarov@gmail.com>
2021-08-17 10:47:24 +0300

    Убрал обработку http ответов

--- src/Provider/AddressProvider.php
+++ src/Provider/AddressProvider.php
@@ -8,33 +8,20 @@ use Jaddek\Bitgo\Http\Client\Client\Wallet\AddressInterface;
 use Jaddek\Bitgo\Http\Client\Client\Wallet\WebhookInterface;
 use Jaddek\Bitgo\Http\Client\Response\AddressDTO;
 use Psr\Log\LoggerInterface;
-use RuntimeException;
-use Symfony\Contracts\HttpClient\Exception\ExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
 
 class AddressProvider
 {
     public function __construct(
         private AddressInterface $client,
-        private LoggerInterface $logger
+        private LoggerInterface  $logger
     )
     {
     }
 
     public function create(string $coin, string $walletId, string $label): AddressDTO
     {
-        try {
-            $response = $this->client->create($coin, $walletId, $label);
+        $response = $this->client->create($coin, $walletId, $label);
 
-            return new AddressDTO($response->toArray());
-        } catch (HttpExceptionInterface $e) {
-            $this->logger->debug($e->getResponse()->getContent(false));
-
-            throw new RuntimeException('Cant create wallet address', 0, $e);
-        } catch (ExceptionInterface $e) {
-            $this->logger->debug($e->getMessage());
-
-            throw new RuntimeException('Cant create wallet address', 0, $e);
-        }
+        return new AddressDTO($response->toArray());
     }
 }
